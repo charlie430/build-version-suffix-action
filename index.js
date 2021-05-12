cd84cd838c6d848e49eb483f66e9c8a355037226
Charles Mathis <chmathis@factset.com>
2021-05-12 14:58:57 -0400

    a

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const buildVersionSuffix = require('./buildVersionSuffix');
 
 async function run() {
   try {
-    core.log('github.context:');
-    core.log(github.context);
+    core.debug('github.context:');
+    core.debug(github.context);
     const versionSuffix = await buildVersionSuffix(github.context);
     core.setOutput('versionSuffix', versionSuffix);
   } catch (error) {
